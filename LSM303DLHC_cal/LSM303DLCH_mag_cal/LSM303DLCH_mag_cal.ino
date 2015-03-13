965ff0143c9e95bcb738da80ebce5ad213d41216
Nate Costello <natecostello@gmail.com>
2015-03-12 21:41:27 -0400

    Completed files for magnetometer calibration.

---
+++ LSM303DLHC_cal/LSM303DLCH_mag_cal/LSM303DLCH_mag_cal.ino
@@ -0,0 +1,75 @@
+// LSM303DLCH_mag_cal.ino
+
+
+// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
+// is used in I2Cdev.h
+#include <Wire.h>
+
+// I2Cdev and LSM303DLHC must be installed as libraries, or else the .cpp/.h files
+// for both classes must be in the include path of your project
+#include "LSM303DLHC.h"
+#include "I2Cdev.h"
+
+LSM303DLHC accelMag;
+
+int16_t mx, my, mz;
+
+void setup() {
+    // join I2C bus (I2Cdev library doesn't do this automatically)
+    Wire.begin();
+
+    // initialize serial communication
+    Serial.begin(9600);
+
+    // initialize device
+    Serial.println("DELETE BEFORE SAVING: Initializing I2C devices...");
+    accelMag.initialize();
+
+    // verify connection
+    Serial.println("DELETE BEFORE SAVING: Testing device connections...");
+    Serial.println(accelMag.testConnection() ? "DELETE BEFORE SAVING: LSM303DLHC connection successful" : "DELETE BEFORE SAVING: LSM303DLHC connection failed");
+
+    // set accel data rate to 200hz
+    accelMag.setAccelOutputDataRate(200);
+
+    // set mag data rate to 220hz
+    accelMag.setMagOutputDataRate(220);
+
+    // set mag gain
+    accelMag.setMagGain(1100);
+
+    // enable mag
+    accelMag.setMagMode(LSM303DLHC_MD_CONTINUOUS);
+
+
+
+}
+
+void loop() {
+    Serial.println("DELETE BEFORE SAVING: Allow device to warm up for a few minutes before proceeding.");
+    delay(1000);
+    Serial.println("DELETE BEFORE SAVING: Send any key over serial, and then proceed to move device through all axes and orientations until data acquisition stops.");
+    
+    while (not Serial.available()){
+        delay(10);
+    }
+    while (Serial.available()){
+        Serial.read();
+    }
+
+    //format for datafile is mx, my, mz
+
+    for (int x = 0; x < 300; x++) {
+        accelMag.getMag(&mx, &my, &mz);
+        Serial.print(mx); Serial.print(',');
+        Serial.print(my); Serial.print(',');
+        Serial.print(mz); Serial.println();
+        delay(100);
+    }
+    Serial.println("DELETE BEFORE SAVING:  Data Acquisition Complete.  Delete all lines marked as DELETE BEFORE SAVING.");
+    Serial.println("DELETE BEFORE SAVING:  Save file as 'magData.txt' and run octave/matlab file to complete computation of calibration constants.");
+    while (true){
+        delay(1000);
+    }
+}
+
---
+++ LSM303DLHC_cal/LSM303DLCH_mag_cal/magCal.m
@@ -0,0 +1,94 @@
+%// Copyright (c) 2015, natecostello
+%// 
+%//
+%// Parts of this file are based on original code as below:
+%//
+%//  Copyright (c) 2014, richards-tech
+%// 
+%//  This file is part of RTEllipsoidFit
+%//
+%//  RTEllipsoidFit is free software: you can redistribute it and/or modify
+%//  it under the terms of the GNU General Public License as published by
+%//  the Free Software Foundation, either version 3 of the License, or
+%//  (at your option) any later version.
+%//
+%//  RTEllipsoidFit is distributed in the hope that it will be useful,
+%//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+%//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%//  GNU General Public License for more details.
+%//
+%//  You should have received a copy of the GNU General Public License
+%//  along with RTEllipsoidFit.  If not, see <http://www.gnu.org/licenses/>.
+%//
+
+%// Parts of this file are based on original code as below:
+
+%******************************************************************************************
+% Magnetometer Calibration Skript for Razor AHRS v1.4.2
+% 9 Degree of Measurement Attitude and Heading Reference System
+% for Sparkfun "9DOF Razor IMU" and "9DOF Sensor Stick"
+%
+% Released under GNU GPL (General Public License) v3.0
+% Copyright (C) 2013 Peter Bartz [http://ptrbrtz.net]
+% Copyright (C) 2012 Quality & Usability Lab, Deutsche Telekom Laboratories, TU Berlin
+% Written by Peter Bartz (peter-bartz@gmx.de)
+%
+% Infos, updates, bug reports, contributions and feedback:
+%     https://github.com/ptrbrtz/razor-9dof-ahrs
+%******************************************************************************************
+
+% This file loads the data captured during execution of LSM303DLCH_accel_cal.ino and calculates
+% the appropriate calibration constants for the device.
+
+% Load data
+fprintf('Loading data ...\n');
+
+data = load('magData.txt');
+
+% parse data into vector x, y, and z
+x = data(:,1);
+y = data(:,2);
+z = data(:,3);
+
+[center, radii, evecs, v] = ellipsoid_fit( [x y z] );
+
+scaleMat = inv([radii(1) 0 0; 0 radii(2) 0; 0 0 radii(3)]) * min(radii); 
+correctionMat = evecs * scaleMat * evecs';
+    
+% now correct the data to show that it works
+
+magVector = [x - center(1), y - center(2), z - center(3)]'; % take off center offset
+magVector = correctionMat * magVector;             % do rotation and scale
+%magVector = evecs * magVector;              % do rotation and scale
+xCorr = magVector(1, :);                    % get corrected vectors
+yCorr = magVector(2, :);
+zCorr = magVector(3, :);
+magVector
+% delete this later
+fprintf('plotting ...');
+scatter3(x,y,z,'g');
+hold on;
+scatter3(xCorr, yCorr, zCorr, 'r');
+hold off;
+%sombrero();
+axis([-800 800 -800 800 -800 800]);
+set (gca (), "plotboxaspectratio", [1 1 1.25]);
+pause();
+
+fprintf('Copy this into your code that calibrates the accelerometer outputs: \n')
+fprintf('float mag11 = %f;\n', correctionMat(1,1));
+fprintf('float mag12 = %f;\n', correctionMat(2,1));
+fprintf('float mag13 = %f;\n', correctionMat(3,1));
+fprintf('float mag10 = %f;\n', center(1));
+
+fprintf('float mag21 = %f;\n', correctionMat(1,2));
+fprintf('float mag22 = %f;\n', correctionMat(2,2));
+fprintf('float mag23 = %f;\n', correctionMat(3,2));
+fprintf('float mag20 = %f;\n', center(2));
+
+fprintf('float mag31 = %f;\n', correctionMat(1,3));
+fprintf('float mag32 = %f;\n', correctionMat(2,3));
+fprintf('float mag33 = %f;\n', correctionMat(3,3));
+fprintf('float mag30 = %f;\n', center(3));
+fprintf('end copy ... \n')
+    
